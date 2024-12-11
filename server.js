0a557f9cb67de57e7947e663656864933d7a4329
Vania <221110551@students.mikroskil.ac.id>
2024-12-11 11:30:18 +0700

    Add Cloud Build configuration

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/news/:id', async(req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
